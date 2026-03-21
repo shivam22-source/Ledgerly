6b71597170b5aaa29c9e6fa54e77a7a5e545d8e0
Shivam <b424056@iiit-bh.ac.in>
2026-03-22 02:04:56 +0530

    update

--- backend/middleware/rate-limit.js
+++ backend/middleware/rate-limit.js
@@ -3,7 +3,7 @@ const Loginlimiter=ratelimit({
   windowMs:15*60*1000,///15 min
   max:3,
   message:{
-    message:"Too many request try again after 14 min"
+    message:"Too many request try again after 15 min"
   }
 })
   
