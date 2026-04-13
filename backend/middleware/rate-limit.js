da983513d569334ae6c73d2489f3d472e799b995
Shivam Thakur <b424056@iiit-bh.ac.in>
2026-04-13 22:04:34 +0530

    Update rate-limit.js

--- backend/middleware/rate-limit.js
+++ backend/middleware/rate-limit.js
@@ -1,7 +1,7 @@
 const ratelimit=require("express-rate-limit")
 const Loginlimiter=ratelimit({
   windowMs:15*60*1000,///15 min
-  max:3,
+  max:10,
   message:{
     message:"Too many request try again after 15 min"
   }
