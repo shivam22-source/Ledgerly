d2c14612685f3884a199ad91c9b033cf16cccde9
Shivam <b424056@iiit-bh.ac.in>
2026-06-23 14:57:30 +0530

    update

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,14 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI is missing");
-  }
-
-  await mongoose.connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 10000,
-  });
-
+  await mongoose.connect(process.env.MONGO_URI);
   console.log("DB connected");
 };
 
